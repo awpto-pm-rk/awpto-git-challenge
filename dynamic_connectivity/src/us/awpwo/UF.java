f0fd92094656418a12c9c298d9691e2f0c59cb3d
awpto-pm-rk <mordredxp+awpto@gmail.com>
2016-09-17 15:24:01 +0200

    Update UF.java

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,9 +3,11 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
+	private int[] sz;
 	
 	public UF(int N) {
 		id = new int[N];
+		sz = new int [N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
@@ -17,8 +19,16 @@ public class UF {
 	
 	public void union(int p, int q) {
 	 	int i = root(p);
-	 	int j = root(q);
-	 	id[i] = j;
+		int j = root(q);
+		if (i == j) return;
+ 		if (sz[i] < sz[j]) {
+ 			id[i] = j;
+ 			sz[j] += sz[i];
+ 		}
+		else {
+ 			id[j] = i;
+ 			sz[i] += sz[j];
+		}
 	}
 	
 	private int root(int i) {
